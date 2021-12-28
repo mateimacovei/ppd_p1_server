d4824073bd5cfe7bd8ab780fba26bef2ec307df4
Matei <mmir2758@scs.ubbcluj.ro>
2021-12-28 18:59:06 +0200

    config

--- src/main/java/com/example/ppd_p1_server/model/Hall.java
+++ src/main/java/com/example/ppd_p1_server/model/Hall.java
@@ -1,8 +1,18 @@
 package com.example.ppd_p1_server.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
-@Ent
+@Entity
+@NoArgsConstructor
 public class Hall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
