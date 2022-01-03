f2347d6622118c62b9adab34110c587057b115af
AndreiMalea <andrei.malea13@gmail.com>
2022-01-03 19:19:42 +0200

    finished client

--- client.py
+++ client.py
@@ -6,26 +6,28 @@ import requests
 from promise import *
 
 
-def myPrint(response, places):
+def myPrint(response, places, showId):
+    print(showId)
     print(places)
     print(response.json())
 
 
 if __name__ == '__main__':
-    for i in range(10):
+#     for i in range(10):
+    while True:
+        showId = random.randint(1,3)
         places = [random.randint(1, 100) for _ in range(5)]
         Promise.resolve(
             requests.post(
                 'http://localhost:8080/reservation',
                 json={
-                    "showId": 1,
-                    # "places": [1, 2, 3]
+                    "showId": showId,
                     "places": places
                 }
             )
         ).then(
-            lambda response: myPrint(response, places)
+            lambda response: myPrint(response, places, showId)
         ).catch(
-            lambda error: logging.error(error)
+            lambda error: print(error)
         )
-        time.sleep(5)
+        time.sleep(2)
